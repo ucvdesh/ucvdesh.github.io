773b1c520fd27dee993f00461b81c2459ee6660b
Daniel Silva <dsilva@evolvtechnology.com>
2024-02-07 22:13:21 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <iframe src="https://my-test-app-gamma-eight.vercel.app/" title="YouTube video player" style={{width: 1200, height: 800}} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
   );
 }
 
