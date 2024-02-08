c00b74ff7e2d605aa7e2649b527d86e857660a08
Daniel Silva <dsilva@evolvtechnology.com>
2024-02-07 21:33:59 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={"https://vercel.com/_next/image?url=%2Fapi%2Fscreenshot%3Fdark%3D1%26deploymentId%3Ddpl_8Vk4opS9gWyBBQz2aEtn9RdFbwDe%26withStatus%3D1&w=640&q=75&dpl=dpl_49mZxBTtr4hZXnsTq7zDcqK9PGim"} className="App-logo" alt="logo" />
+        <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png"} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
