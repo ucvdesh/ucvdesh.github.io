d47f49556e086da0d6876b10a5328255bd67a8ba
Daniel Silva <dsilva@evolvtechnology.com>
2024-02-15 15:56:15 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  React.useEffect(() => {
+    console.log("geotab", geotab.addin)
+  }, [])
+
   return (
     <iframe src="https://my-test-app-gamma-eight.vercel.app/" title="YouTube video player" style={{width: "100%", height: "100vh"}} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
   );
