7190a2b082aece79529d961255396c5999d90d60
Daniel Silva <dsilva@evolvtechnology.com>
2024-02-16 12:59:23 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,71 @@ function App() {
   const [api, setApi] = useState(null);
 
   useEffect(() => {
-    api.call('Get', {
-      typeName: 'FuelUsed'
-    }).then((test) => {
-      console.log('test', test);
-    }).catch((message, error) => {
-      console.log('Something went wrong', message, error);
-    })
+    if (window && window.geotab) {
+
+    window.geotab.addin.moveEv = function () {
+
+      return {
+
+        /**
+         * initialize() is called only once when the Add-In is first loaded. Use this function to initialize the
+         * Add-In's state such as default values or make API requests (MyGeotab or external) to ensure interface
+         * is ready for the user.
+         * @param {object} freshApi - The GeotabApi object for making calls to MyGeotab.
+         * @param {object} freshState - The page state object allows access to URL, page navigation and global group filter.
+         * @param {function} initializeCallback - Call this when your initialize route is complete. Since your initialize routine
+         *        might be doing asynchronous operations, you must call this method when the Add-In is ready
+         *        for display to the user.
+         */
+        initialize: function (freshApi, freshState, initializeCallback) {
+
+          const test = freshApi.call('Get', {
+            typeName: 'FuelUsed'
+          });
+
+          console.log('test', test);
+
+          console.log('initializeTriggered');
+
+          initializeCallback();
+        },
+
+        /**
+         * focus() is called whenever the Add-In receives focus.
+         *
+         * The first time the user clicks on the Add-In menu, initialize() will be called and when completed, focus().
+         * focus() will be called again when the Add-In is revisited. Note that focus() will also be called whenever
+         * the global state of the MyGeotab application changes, for example, if the user changes the global group
+         * filter in the UI.
+         *
+         * @param {object} freshApi - The GeotabApi object for making calls to MyGeotab.
+         * @param {object} freshState - The page state object allows access to URL, page navigation and global group filter.
+         */
+        focus: function (freshApi, freshState) {
+
+          // show main content
+
+        },
+
+        /**
+         * blur() is called whenever the user navigates away from the Add-In.
+         *
+         * Use this function to save the page state or commit changes to a data store or release memory.
+         *
+         * @param {object} freshApi - The GeotabApi object for making calls to MyGeotab.
+         * @param {object} freshState - The page state object allows access to URL, page navigation and global group filter.
+         */
+        blur: function () {
+          // hide main content
+        }
+      };
+    };
+    }
   }, []);
 
 
   return (
-    <div>Test</div>
+    <div id="moveEv">Test</div>
   );
 }
 
