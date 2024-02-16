6f3d1cb47b187ebd86b8410674dec60a3e64f236
Daniel Silva <dsilva@evolvtechnology.com>
2024-02-16 12:49:02 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,31 +7,13 @@ function App() {
   const [api, setApi] = useState(null);
 
   useEffect(() => {
-    const initializeAddIn = async (api) => {
-      // Aquí estableces el objeto api en el estado cuando Geotab lo pasa a tu Add-In
-      const res = await api.call('Get', {
-        typeName: 'Device',
-        search: {
-          id: 'b1'
-        }
-      }, (result) => {
-        console.log(result);
-      })
-
-      console.log(res)
-      setApi(api);
-    };
-
-
-
-    // Simula la inicialización de Geotab llamando a tu función de inicialización
-    // En un caso real, Geotab llamaría a esta función y pasaría el objeto `api`
-    window.onload = () => {
-      // Esto es solo un ejemplo. Deberías reemplazarlo con la lógica real de inicialización proporcionada por Geotab.
-      if (typeof window !== "undefined" && window.geotab) {
-        window.geotab.initialize(initializeAddIn);
-      }
-    };
+    api.call('Get', {
+      typeName: 'FuelUsed'
+    }).then((test) => {
+      console.log('test', test);
+    }).catch((message, error) => {
+      console.log('Something went wrong', message, error);
+    })
   }, []);
 
 
