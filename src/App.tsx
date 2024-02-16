9936600cbec1cfd09d138950bbe31e3722dbbd31
Daniel Silva <dsilva@evolvtechnology.com>
2024-02-16 12:22:39 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,39 @@
 // @ts-nocheck
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  const foo = useCallback( () => {
-
-    if (typeof window !== "undefined") {
-      window.onload = function() {
-        // Geotab podría pasar el objeto api como argumento a una función de inicialización
-        initializeAddIn(async (api, state) => {
-          console.log(window)
-          console.log("pase por aqui", api, state)
-          const result = await api.call('Get', {
-            typeName: 'Device'
-          });
-          console.log('result', result);
-
-          console.log('initializeTriggered');
-          return result;
-        });
-      }
-    }
-    return false;
-  }, [])
+  const [api, setApi] = useState(null);
 
   useEffect(() => {
-    foo()
+    const initializeAddIn = async (api) => {
+      // Aquí estableces el objeto api en el estado cuando Geotab lo pasa a tu Add-In
+      const res = await api.call('Get', {
+        typeName: 'Device',
+        search: {
+          id: 'b1'
+        }
+      }, (result) => {
+        console.log(result);
+      })
+
+      console.log(res)
+      setApi(api);
+    };
+
+
+
+    // Simula la inicialización de Geotab llamando a tu función de inicialización
+    // En un caso real, Geotab llamaría a esta función y pasaría el objeto `api`
+    window.onload = () => {
+      // Esto es solo un ejemplo. Deberías reemplazarlo con la lógica real de inicialización proporcionada por Geotab.
+      if (typeof window !== "undefined" && window.geotab) {
+        window.geotab.initialize(initializeAddIn);
+      }
+    };
   }, []);
+
 
   return (
     <iframe src="https://my-test-app-gamma-eight.vercel.app/" title="YouTube video player" style={{width: "100%", height: "100vh"}} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
