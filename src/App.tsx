f65325d6424eec94a2e5d8323ad3121e8085a80c
Daniel Silva <dsilva@evolvtechnology.com>
2024-02-16 12:24:29 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ function App() {
 
 
   return (
-    <iframe src="https://my-test-app-gamma-eight.vercel.app/" title="YouTube video player" style={{width: "100%", height: "100vh"}} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"></iframe>
+    <div>Test</div>
   );
 }
 
